17b566059883b1a8be6d697e909f47eebc9b623a
MTroza <michaeltroza@gmail.com>
2026-03-17 09:45:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-function doGet(e) {
-
-var sheet = SpreadsheetApp.getActiveSpreadsheet();
-var usersSheet = sheet.getSheetByName("USERS");
-
-var action = e.parameter.action;
-
-if(action == "login"){
-
-var username = e.parameter.username;
-var password = e.parameter.password;
-
-var data = usersSheet.getDataRange().getValues();
-
-for(var i=1; i<data.length; i++){
-
-var user = data[i][1];
-var pass = data[i][2];
-
-if(user == username && pass == password){
-
-return ContentService.createTextOutput("success");
-
-}
-
-}
-
-return ContentService.createTextOutput("failed");
-
-}
-
-return ContentService.createTextOutput("no action");
-
-}
+fetch("YOUR_WEB_APP_URL?action=login&username=" + user + "&password=" + pass)
+  .then(res => res.text())
+  .then(data => {
+    if (data == "success") {
+      window.location.href = "dashboard.html";
+    } else {
+      document.getElementById("error").innerText = "Invalid Username or Password";
+    }
+  });
